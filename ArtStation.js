5a8e0b2f3ad324325466fb6790c3706312d68a81
okita925 <60242953+okita925@users.noreply.github.com>
2020-02-13 22:08:15 +0900

    Create ArtStation.js

---
+++ ArtStation.js
@@ -0,0 +1,21 @@
+
+
+$(function(){
+    $("#modal-open").click(
+        function(){
+            $("#login-modal").fadeIn();
+            return false;
+        });
+});
+
+$(function(){
+    $("#modal-open").click(
+        function(){
+            $("#glaylayer").show();
+            return false;
+        });
+});
+
+
+
+
